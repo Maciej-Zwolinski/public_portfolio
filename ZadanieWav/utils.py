c8a757f28cad5f001bd0328974f9bcf6b55a8bdf
Maciej Zwoliński <maciej.a.zwolinski@gmail.com>
2019-03-01 02:11:11 +0100

    autopep

--- ZadanieWav/utils.py
+++ ZadanieWav/utils.py
@@ -1,116 +1,130 @@
-import numpy as np
 import os
+import logging
+import numpy as np
+
 
 def wav_to_array(file_name):
     """Reads .wav file and loads data into np.array"""
 
+    logger = logging.getLogger(__name__)
     with open(file_name, "rb") as f:
-        f.seek(16,0)
-        n=int.from_bytes(f.read(4), byteorder='little')
-        # print(n)
-        f.seek(2,1)
+        f.seek(16, 0)
+        n = int.from_bytes(f.read(4), byteorder='little')
+        logger.debug(n)
+        f.seek(2, 1)
         num_channels = int.from_bytes(f.read(2), byteorder='little')
-        f.seek(8,1)
-        Byte_rate = int(int.from_bytes(f.read(2), byteorder='little')/num_channels)
-        # print(Byte_rate)
-        f.seek(24,0)
-        sample_rate=int.from_bytes(f.read(4), byteorder='little')
-        #print(sample_rate)
-        f.seek(16+n+8,0)
-        data_l = int.from_bytes(f.read(4), byteorder='little')/Byte_rate
-        # print(int(data_l/num_channels))
-        if Byte_rate==1: d_t=np.uint8
-        elif Byte_rate==2: d_t=np.int16
-        data=np.zeros((int(num_channels),int(data_l/num_channels)),dtype=d_t)
+        f.seek(8, 1)
+        byte_rate = int(int.from_bytes(f.read(2), byteorder='little') / num_channels)
+        logger.debug(byte_rate)
+        f.seek(24, 0)
+        sample_rate = int.from_bytes(f.read(4), byteorder='little')
+        logger.debug(sample_rate)
+        f.seek(16 + n + 8, 0)
+        data_l = int.from_bytes(f.read(4), byteorder='little') / byte_rate
+        logger.debug(int(data_l/num_channels))
+        if byte_rate == 1:
+            d_t = np.uint8
+        elif byte_rate == 2:
+            d_t = np.int16
+        data = np.zeros(
+            (int(num_channels), int(
+                data_l / num_channels)), dtype=d_t)
         for k in range(int(num_channels)):
-            for i in range(int(data_l/num_channels)):
-                data[k][i]= np.int16(int.from_bytes(f.read(Byte_rate), byteorder='little'))
-        
+            for i in range(int(data_l / num_channels)):
+                data[k][i] = np.int16(int.from_bytes(f.read(byte_rate), byteorder='little'))
+
     return num_channels, sample_rate, data
 
 
 def array_abs(data):
     """Scales data by max value of a type"""
-    
-    x=np.zeros((data.shape[0],data.shape[1]), dtype=np.float32)
+
+    x = np.zeros((data.shape[0], data.shape[1]), dtype=np.float32)
     for k in range(data.shape[0]):
-        q=32768 #max int16 value
-        x[k][:]=np.float32(np.divide(data[k][:],q))
+        q = 32768  # max int16 value
+        x[k][:] = np.float32(np.divide(data[k][:], q))
 
     return x
+
 
 def array_relative(data):
     """Scales data by maximal occuring value"""
-    
-    x=np.zeros((data.shape[0],data.shape[1]), dtype=np.float32)
+
+    x = np.zeros((data.shape[0], data.shape[1]), dtype=np.float32)
     for k in range(data.shape[0]):
-        q=np.amax([np.amax(data[k][:]),np.abs(np.amin(data[k][:]))])
-        x[k][:]=np.float32(np.divide(data[k][:],q))
+        q = np.amax([np.amax(data[k][:]), np.abs(np.amin(data[k][:]))])
+        x[k][:] = np.float32(np.divide(data[k][:], q))
 
     return x
+
 
 def avg_zerolike_density(data, margin=5, span=40):
     """Calculates density of 'low' signal values within given span over entire data"""
-    
-    data_copy=data.copy()
+
+    data_copy = data.copy()
     for k in range(data.shape[0]):
         for l in range(data.shape[1]):
-            data_copy[k][l]=(abs(data[k][l])<=abs(margin))
+            data_copy[k][l] = (abs(data[k][l]) <= abs(margin))
 
-    x=np.zeros((data.shape[0],data.shape[1]-span), dtype=np.float32)
+    x = np.zeros((data.shape[0], data.shape[1] - span), dtype=np.float32)
     for k in range(x.shape[0]):
         for l in range(x.shape[1]):
-            x[k][l]=np.float32(np.sum(data_copy[k][ l : l+span ])/span)
+            x[k][l] = np.float32(np.sum(data_copy[k][l: l + span]) / span)
 
     return x
 
-def edge_detection(x,span=40):
+
+def edge_detection(x, span=40):
     """Detects edges between 'high' and 'low' signal regions"""
-    
-    h=list()
+
+    h = list()
 
     for k in range(x.shape[0]):
-        flg=0
-        g=list()
-        for i in range(x.shape[1]-1):
-            if (flg==0 and x[k][i]>=0.5):
-                flg=1
-                g.append(i+span//2)
-            elif (flg==1 and x[k][i]<0.5):
-                flg=0
-                g.append(i+span//2)
+        flg = 0
+        g = list()
+        for i in range(x.shape[1] - 1):
+            if (flg == 0 and x[k][i] >= 0.5):
+                flg = 1
+                g.append(i + span // 2)
+            elif (flg == 1 and x[k][i] < 0.5):
+                flg = 0
+                g.append(i + span // 2)
         h.append(g)
 
     return h
 
-def edge_cleanup(h,sample_rate,tmax=0.1,suspend=100):
+
+def edge_cleanup(h, sample_rate, tmax=0.1, suspend=100):
     """Pairs up rising and falling edges
        Concatenates neraby 'low' regions
        Checks if concatenated regions aren't too long"""
 
+    logger = logging.getLogger(__name__)
     for k in range(len(h)):
-        g=h[k]
-        f=list()
-        l=list()
-        for i in range(0,len(g),2):
-            if g[i]!=g[-1]:
-                t=g[i+1] - g[i]
-                if t<tmax*sample_rate and t>suspend:
-                    f.append([g[i], g[i+1]])
-        # print(f)
-        i=0
-        while i <len(f)-1:
-            if f[i+1][0]-f[i][1]<suspend:
-                f[i][1]=f[i+1][1]
-                f.pop(i+1)
-            else: i+=1
+        g = h[k]
+        f = list()
+        l = list()
+        for i in range(0, len(g), 2):
+            if g[i] != g[-1]:
+                t = g[i + 1] - g[i]
+                if t < tmax * sample_rate and t > suspend:
+                    f.append([g[i], g[i + 1]])
+        logger.debug(f)
+        i = 0
+        while i < len(f) - 1:
+            if f[i + 1][0] - f[i][1] < suspend:
+                f[i][1] = f[i + 1][1]
+                f.pop(i + 1)
+            else:
+                i += 1
 
-        # print(f)
-        i=0
-        while i<len(f) and len(f)!=0:
-            if f[i][1]-f[i][0]>tmax*sample_rate:
+        logger.debug(f)
+        i = 0
+        while i < len(f) and f:
+            if f[i][1] - f[i][0] > tmax * sample_rate:
                 f.pop(i)
-            else: i+=1
+            else:
+                i += 1
 
         l.append(f)
 
--- ZadanieWav/wav_eval.py
+++ ZadanieWav/wav_eval.py
@@ -13,45 +13,49 @@ logging.basicConfig(
 
 def gen_low_regions(file):
     """Returns a list of 'low' regions boundries"""
-    n_channel, sample_rate, data=utils.wav_to_array(file)
-    p=list()
+    n_channel, sample_rate, data = utils.wav_to_array(file)
+    p = list()
     for span in args.span:
-        x=utils.avg_zerolike_density(data,span=span)
-        h=utils.edge_detection(x,span=span)
-        g=utils.edge_cleanup(h,sample_rate)
+        x = utils.avg_zerolike_density(data, span=span)
+        h = utils.edge_detection(x, span=span)
+        g = utils.edge_cleanup(h, sample_rate)
         p.append(copy.deepcopy(g))
 
     return p, n_channel
 
+
 def boundry_stability_check(p, n_channel):
     """Returns boundries that are stable across all spans"""
-    while len(p)>1:
+    while len(p) > 1:
         for k in range(n_channel):
-            i=0
+            i = 0
             while i < len(p[0][k]):
                 if not (p[0][k][i] in p[1][k][:]):
                     p[0][k].pop(i)
-                else: i+=1
+                else:
+                    i += 1
             p[1].pop(k)
         p.pop(1)
 
     return p
 
+
 def set_flags(p, n_channel):
     """Sets corruption flags for channels"""
-    flg=list()
+    flg = list()
     for k in range(n_channel):
-        if len(p[0][k])==0:
+        if p[0][k]:
+            flg.append(0)
+        else:
             flg.append(1)
-        else: flg.append(0)
 
     return flg
-    
+
 
 def main(args):
     logger = logging.getLogger(__name__)
     logger.debug(args.span)
-    assert len(args.span)>2, "Span too short, at least 2 elements required"
+    assert len(args.span) > 2, "Span too short, at least 2 elements required"
 
     for file in os.listdir(args.datadir):
         if file.endswith('.wav'):
@@ -59,21 +63,20 @@ def main(args):
             p = boundry_stability_check(p, n_channel)
             flg = set_flags(p, n_channel)
 
-            if (1 in flg) and not(0 in flg):
+            if (1 in flg) and (0 not in flg):
                 logger.info(f'[{file}][valid]')
                 continue
             elif (1 in flg) and (0 in flg):
-                out=f'[{file}][partially valid]'
-            else: 
-                out=f'[{file}][invalid]'
+                out = f'[{file}][partially valid]'
+            else:
+                out = f'[{file}][invalid]'
 
             for k in range(n_channel):
-                if flg[k]==1:
-                    out+='[NaN]'
+                if flg[k] == 1:
+                    out += '[NaN]'
                 else:
-                    out+=f'[{p[0][k][0][0]}]'
+                    out += f'[{p[0][k][0][0]}]'
 
-            
             logger.info(f"{out}")
 
     os.system("pause")
@@ -82,14 +85,16 @@ def main(args):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--datadir',
-                        help='Directory to be chaecked for .wav files. Default "./"',
-                        default ='./')
-    parser.add_argument('--span',
-                        nargs='+',
-                        type=int,
-                        help='list of spans across which we check for stability',
-                        default = [30,40,50,60])
+    parser.add_argument(
+        '--datadir',
+        help='Directory to be chaecked for .wav files. Default "./"',
+        default='./')
+    parser.add_argument(
+        '--span',
+        nargs='+',
+        type=int,
+        help='list of spans across which we check for stability',
+        default=[30, 40, 50, 60])
     args = parser.parse_args()
 
     sys.exit(main(args))
