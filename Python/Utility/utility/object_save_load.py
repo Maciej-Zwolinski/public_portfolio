cf367e1b6e4b6cef3952ac83f2a2f1a6dc6900d1
Maciej-Zwolinski <46869207+Maciej-Zwolinski@users.noreply.github.com>
2020-05-27 19:16:26 +0200

    Utility update
    updated utility package, tests indicate it works

--- Python/Utility/utility/object_save_load.py
+++ Python/Utility/utility/object_save_load.py
@@ -1,16 +1,30 @@
 
 def load_object_internal_state_from_dict(obj, dictionary):
     """
+    this function is not supposed to be used to initialize objects
     if a key represents simple variable then it's value is directly loaded
-    if a key represents an object (has a method 'load_internal_state') that method is called
-        this seems redundant, but otherwise there is no way of distinguishing between attribute that's an object and a dict
+    if en existing attribute is a dict, then the value is copied without unpacking
+    an attempt is made to further unpack dictionary, if it fails, it is assumed the dictionary has been unpacked fully
     """
     for key in dictionary.keys():
+        if hasattr(obj, key):
+            if isinstance(getattr(obj, key), dict):
+                setattr(obj, key, dictionary.get(key))
+            else:
+                try:
+                    load_object_internal_state_from_dict(getattr(obj, key), dictionary.get(key))
+                except AttributeError:
+                    setattr(obj, key, dictionary.get(key))
+        '''
+        # old version, kept for reference
+        #if a key represents an object (has a method 'load_internal_state') that method is called
+        #this seems redundant, but otherwise there is no way of distinguishing between attribute that's an object and a dict
         if hasattr(obj, key):
             try:
                 getattr(obj, key).load_internal_state(dictionary.get(key))
             except AttributeError:
                 setattr(obj, key, dictionary.get(key))
+        '''
 
 
 def save_object_internal_state_to_dict(obj, *keys):
@@ -55,11 +69,8 @@ if __name__ == '__main__':
         def default_save(self):
             return save_object_internal_state_to_dict(self, 'z')
 
-        def load_internal_state(self, _dictionary):
-            load_object_internal_state_from_dict(self, _dictionary)
-
         def __str__(self):
-            return 'Obj2: ' + str(self.z) + ', '
+            return 'Obj1: ' + str(self.z) + ', '
 
 
     class Obj2:
@@ -69,11 +80,8 @@ if __name__ == '__main__':
         def default_save(self):
             return save_object_internal_state_to_dict(self, 'x', {'y': ()})
 
-        def load_internal_state(self, _dictionary):
-            load_object_internal_state_from_dict(self, _dictionary)
-
         def __str__(self):
-            return 'Obj3: ' + str(self.x) + ' ' + str(self.y) + ', '
+            return 'Obj2: ' + str(self.x) + ' ' + str(self.y) + ', '
 
 
     class Obj3:
@@ -86,7 +94,7 @@ if __name__ == '__main__':
             return save_object_internal_state_to_dict(self, 'attr1')
 
         def __str__(self):
-            return 'Obj1: ' + str(self.attr1) + '  ' + str(self.attr2) + ' ' + str(self.attr3)
+            return 'Obj3: ' + str(self.attr1) + '  ' + str(self.attr2) + ' ' + str(self.attr3)
 
 
     obj = Obj3(3, 1, 2, 7)
